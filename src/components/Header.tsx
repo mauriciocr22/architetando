ea3971889539744065673e39837bf168e78b81dd
Maurício de Carvalho <mauriciocr223@gmail.com>
2025-05-13 19:11:27 -0300

    feat(header): create initial header

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import menuImg from "../assets/menu.png";
+import logoImg from "../assets/logo-blue.png";
+
+export default function Header() {
+    return (
+        <header className="w-full h-36">
+            <div className="w-full h-[75%] p-9 px-10 flex items-center justify-between">
+                <div className="flex items-center hover:cursor-pointer">
+                    <img src={menuImg} className="w-9" alt="icone de menu" />
+                    <span className="text-xl">Menu</span>
+                </div>
+                <img src={logoImg} className="" alt="Logo Architetando" />
+                <span className="w-[87px]"></span>
+            </div>
+            <nav className="w-full h-[25%] bg-[#080520] flex items-center justify-center">
+                <ul className="flex text-white justify-center gap-36 text-center font-semibold">
+                    <li><a href="">Página Inicial</a></li>
+                    <li><a href="">Cursos</a></li>
+                    <li><a href="">Podcast</a></li>
+                    <li><a href="">Nossos Projetos</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
