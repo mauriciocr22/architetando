56fc6e0667c7a4f7f220b5b76486136b8b74cc91
Maurício Carvalho <mauriciocr223@gmail.com>
2025-05-19 10:21:49 -0300

    feat(home): create home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,12 @@ export default function Header() {
             <div className="w-full h-[75%] p-9 px-10 flex items-center justify-between">
                 <div className="flex items-center hover:cursor-pointer">
                     <img src={menuImg} className="w-9" alt="icone de menu" />
-                    <span className="text-xl">Menu</span>
                 </div>
-                <img src={logoImg} className="" alt="Logo Architetando" />
+                <img src={logoImg} className="w-44" alt="Logo Architetando" />
                 <span className="w-[87px]"></span>
             </div>
             <nav className="w-full h-[25%] bg-[#080520] flex items-center justify-center">
-                <ul className="flex text-white justify-center gap-36 text-center font-semibold">
+                <ul className="flex text-white justify-center gap-36 text-center">
                     <li><a href="">Página Inicial</a></li>
                     <li><a href="">Cursos</a></li>
                     <li><a href="">Podcast</a></li>
