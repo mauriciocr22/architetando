67661e7e00fd41759fa66246a9d9734f56f4a90e
Maurício de Carvalho <mauriciocr223@gmail.com>
2025-05-22 02:56:49 -0300

    feat(Header): fix header css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,16 @@ import logoImg from "../assets/logo-blue.png";
 
 export default function Header() {
     return (
-        <header className="w-full h-36">
-            <div className="w-full h-[75%] p-9 px-10 flex items-center justify-between">
+        <header className="w-full">
+            <div className="w-full h-32 p-9 px-16 flex items-center justify-between">
                 <div className="flex items-center hover:cursor-pointer">
                     <img src={menuImg} className="w-9" alt="icone de menu" />
                 </div>
                 <img src={logoImg} className="w-44" alt="Logo Architetando" />
                 <span className="w-[87px]"></span>
             </div>
-            <nav className="w-full h-[25%] bg-[#080520] flex items-center justify-center">
-                <ul className="flex text-white justify-center gap-36 text-center">
+            <nav className="w-full h-10 bg-[#080520] flex items-center justify-center">
+                <ul className="flex text-white text-lg justify-center gap-40 text-center">
                     <li><a href="">Página Inicial</a></li>
                     <li><a href="">Cursos</a></li>
                     <li><a href="">Podcast</a></li>
