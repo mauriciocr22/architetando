85ae8d8277459498fd50c30506dfb17750709296
Maurício de Carvalho <mauriciocr223@gmail.com>
2025-05-29 12:46:27 -0300

    feat(cursos): add cursos page responsiveness

--- src/pages/Cursos/index.tsx
+++ src/pages/Cursos/index.tsx
@@ -5,12 +5,12 @@ import chevronRight from "../../assets/icones/chevron-right.png"
 
 export default function Cursos() {
     return (
-        <section className="flex justify-center flex-col items-center p-8 gap-8">
+        <section className="flex justify-center flex-col items-center p-8 gap-8 lg:mb-0 mb-21">
             <h1 className="text-4xl font-bold">Nossos Cursos</h1>
             <Link to="/curso-de-capacitação-em-patrimônio" className="border rounded-lg flex items-center h-full">
                 <img src={cursoImg} className="p-2" alt="" />
-                <span className="text-3xl pr-4 font-semibold">Curso de Capacitação em Patrimônio Material das Cidades</span>
-                <div className="bg-[#09043A] h-[107px] w-[107px] flex items-center justify-center"><img src={chevronRight} alt="" /></div>
+                <span className="lg:text-3xl text-lg pr-4 font-semibold">Curso de Capacitação em Patrimônio Material das Cidades</span>
+                <div className="bg-[#09043A] h-[107px] w-[107px] rounded-r-lg flex items-center justify-center"><img src={chevronRight} alt="" /></div>
             </Link>
         </section>
     )
