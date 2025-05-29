e8aba82372ce539fab32f3c6150474df67767015
Maurício de Carvalho <mauriciocr223@gmail.com>
2025-05-29 10:56:47 -0300

    chore(footer): add social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,8 @@ export default function Footer() {
                     <p>ARCHITETANDO ARQUITETURA, AUDIO VISUAL E CAPACITAÇÃO PROFISSIONAL - EIRELI é constituída como pessoa jurídica de direito privado, inscrito no CNPJ n. º 40.144.015/0001-48. Tem como seu responsável técnico RICARDO ANDALAFT - arquiteto e urbanista - CAU-SP nº A20729-2, estabelecida à Praça Visconde de Mauá, 29, sala 422, Centro Histórico, Santos, SP - CEP: 11010-000</p>
                 </div>
                 <ul className="flex w-[70px] gap-2">
-                    <li><a href=""><img src={facebookLogo} alt="" className="hover:opacity-80 transition-opacity duration-100" /></a></li>
-                    <li><a href=""><img src={instagramLogo} alt="" className="hover:opacity-80 transition-opacity duration-100"/></a></li>
+                    <li><a target="_blank" href="https://www.facebook.com/architetando.ummundomelhor"><img src={facebookLogo} alt="" className="hover:opacity-80 transition-opacity duration-100" /></a></li>
+                    <li><a target="_blank" href="https://www.instagram.com/architetando_ummundomelhor"><img src={instagramLogo} alt="" className="hover:opacity-80 transition-opacity duration-100"/></a></li>
                 </ul>
         </footer>
     )
