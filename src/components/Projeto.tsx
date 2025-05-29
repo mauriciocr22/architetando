d91bbc5972f82af72c36e4f8ed1ba364c8d120e5
Maurício de Carvalho <mauriciocr223@gmail.com>
2025-05-29 13:06:03 -0300

    feat(projetos): add projetos page responsiveness

--- src/components/Projeto.tsx
+++ src/components/Projeto.tsx
@@ -7,10 +7,10 @@ interface ProjetoProps {
 export default function Projeto({ imagensProjeto, nomeProjeto, ehUltimo }: ProjetoProps) {
     return (
         <div className="w-full">
-            <h2 className="text-3xl font-bold">{nomeProjeto}</h2>
-            <div className={!ehUltimo ? "flex pb-4 mb-6 border-b-2 border-[#09043A]" : "flex"}>
+            <h2 className="lg:text-3xl text-2xl font-bold mb-4">{nomeProjeto}</h2>
+            <div className={!ehUltimo ? "flex pb-4 mb-14 border-b-2 border-[#09043A]" : "flex"}>
                 {imagensProjeto.map((src, index) => (
-                    <img key={index} className="max-w-[45%] 2xl:max-w-[100%]" src={src} alt={`Imagem ${index + 1}`} />
+                    <img key={index} className="max-w-[50%] 2xl:max-w-[100%]" src={src} alt={`Imagem ${index + 1}`} />
                 ))}
             </div>
         </div>
